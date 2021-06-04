c7c9944429ccc965e6a74bc0498484937f0ad23e
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2021-06-05 00:09:30 +0900

    photoモデルのバリデーションとアソシエーション、active_hashの導入

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,11 @@
 class Photo < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :price
+  belongs_to :user
+
+  has_one_attached :snap
+  validates :title, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :price_id, numericality: { other_than: 1, message: 'Select' }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :photos
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, on: :create
   
