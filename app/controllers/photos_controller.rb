78ca7ee6347a3cb52f7651a0f02599cc8fa78518
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2021-06-13 04:14:55 +0900

    current_user以外の表示制限

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,5 @@
 class PhotosController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @photos = Photo.includes(:user).order('created_at DESC')
