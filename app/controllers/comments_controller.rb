3958073a98f43ae70e85a98ac75dcfd307fccb94
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2021-07-01 03:44:06 +0900

    コメント削除後のリダイレクト先設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,16 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: :destroy
+  before_action :move_to_referer, only: :destroy
+
   def create
     comment = Comment.create(comment_params)
     redirect_to "/photos/#{comment.photo.id}"
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to request.referer
   end
 
   private
@@ -9,4 +18,15 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, photo_id: params[:photo_id])
   end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def move_to_referer
+    redirect_to request.referer and return unless user_signed_in?
+
+    redirect_to request.referer unless @comment.user == current_user
+  end
+
 end
