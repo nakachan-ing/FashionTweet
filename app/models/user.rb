ca39668f11d85832e2de554ac4cfdbd1502690af
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2021-07-03 11:28:14 +0900

    SNS認証実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,19 @@ class User < ApplicationRecord
   has_many :photos, dependent: :destroy
   has_many :comments
   has_many :sns_credentials
+  
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns } 
+  end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, on: :create
