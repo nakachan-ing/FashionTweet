a622d52418004dd8fc03ae40562797e91257fe0f
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2021-07-03 10:31:42 +0900

    "Revert "deviseの再設定""
    This reverts commit c6d87a190398ab7c1680a5df51ad1ac88c852d13.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   get 'search/search'
-  devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks',
-    registrations: 'users/registrations'
-  }
+  devise_for :users
   root to: 'photos#index'
   resources :photos do
     resources :comments, only: [:create, :destroy]
